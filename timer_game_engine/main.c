70db5e00ea46b9557db2d5310131aef28d662113
Paul Griffiths <mail@paulgriffiths.net>
2014-05-11 17:14:36 -0400

    Add auto-generated files.

---
+++ timer_game_engine/main.c
@@ -0,0 +1,24 @@
+/*!
+ * \file            main.c
+ * \brief           Main function for timer_game_engine.
+ * \details         Main function for timer_game_engine.
+ * \author          Paul Griffiths
+ * \copyright       Copyright 2013 Paul Griffiths. Distributed under the terms
+ * of the GNU General Public License. <http://www.gnu.org/licenses/>
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*!
+ * \brief       Main function.
+ * \details     Main function.
+ * \returns     Exit status.
+ */
+
+int main(void) {
+
+    return EXIT_SUCCESS;
+}
